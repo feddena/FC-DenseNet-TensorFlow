36f96f129fc93794907ee4cca18714909dfc3619
fedor.denisov <fedor.denisov@corp.mail.ru>
2019-04-06 08:55:05 +0300

    use jpg images too

--- helpers.py
+++ helpers.py
@@ -4,7 +4,7 @@ def get_data_paths_list(image_folder, mask_folder):
     """Returns lists of paths to each image and mask."""
 
     image_paths = [os.path.join(image_folder, x) for x in os.listdir(
-        image_folder) if x.endswith(".png")]
+        image_folder) if x.endswith(".png") or x.endswith(".jpg") or x.endswith(".JPG")]
     mask_paths = [os.path.join(mask_folder, os.path.basename(x))
                   for x in image_paths]
 
